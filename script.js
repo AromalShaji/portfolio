47b10a36da00fdfdd5283243240c7fea72486461
AromalShaji <92853159+AromalShaji@users.noreply.github.com>
2023-08-22 12:23:57 +0530

    update

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
-var wordflick = function () {
-    setInterval(function () {
-        if (forwards) {
-            if (offset >= words[i].length) {
-                ++skip_count;
-                if (skip_count == skip_delay) {
-                    forwards = false;
-                    skip_count = 0;
-                }
-            }
-        }
-        else {
-            if (offset == 0) {
-                forwards = true;
-                i++;
-                offset = 0;
-                if (i >= len) {
-                    i = 0;
-                }
-            }
-        }
-        part = words[i].substr(0, offset);
-        if (skip_count == 0) {
-            if (forwards) {
-                offset++;
-            }
-            else {
-                offset--;
-            }
-        }
-        $('.word').text(part);
-    }, speed);
-};
+const imageElement = document.getElementById("current-image");
+const images = [
+    "WhatsApp Image 2023-08-21 at 23.57.57-fotor-bg-remover-202308220156.png",
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png"
+];
 
-$(document).ready(function () {
-    wordflick();
-});
+let currentIndex = 0;
+
+function displayNextImage() {
+    imageElement.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Display the first image
+displayNextImage();
+
+// Change image every 5 seconds
+setInterval(displayNextImage, 1000);
